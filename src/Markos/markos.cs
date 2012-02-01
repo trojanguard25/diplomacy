ee66083637369d9554ca8952900464a57383649c
Dan Rausch <dan.rausch@gmail.com>
2012-01-31 22:07:48 -0500

    More cleanup, and added the Markos bot.

---
+++ src/Markos/markos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Polarsoft.Diplomacy;
+using Polarsoft.Diplomacy.Orders;
+using Polarsoft.Diplomacy.Daide;
+
+namespace Polarsoft.Diplomacy.AI
+{
+    class markos : BaseBot
+    {
+        Form parent;
+
+        public markos(string host, int port, Form parent)
+            : base()
+        {
+            this.parent = parent;
+            base.Connect(host, port, "Markos", "0.1");
+        }
+    }
+}
